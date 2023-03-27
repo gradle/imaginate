c40bb7ff7959a4f7fcaa9b5f56f6a6be06bb31c6
Paul Merlin <paul@gradle.com>
2023-03-27 10:46:52 +0200

    Add basic build setup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,10 +2,29 @@ pluginManagement {
     includeBuild("build-logic")
 }
 
+plugins {
+    `gradle-enterprise`
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+}
+
+rootProject.name = "gradle-kotlinconf-2023-app"
+
 includeBuild("domain-library")
 
 include("common-logic")
 include("desktop-app")
 include("android-app")
 include("web-app")
+
+val isCI = System.getenv("CI") == "true"
+if (isCI) {
+    gradleEnterprise {
+        buildScan {
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+            publishAlways()
+            tag("CI")
+        }
+    }
+}
 
