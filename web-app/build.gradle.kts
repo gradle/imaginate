d6f627fdb536b830d2311bbb35004c447f414888
Paul Merlin <paul@gradle.com>
2023-04-02 16:07:01 +0200

    Add :web-app

---
+++ web-app/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    alias(libs.plugins.kotlin.mpp)
+    alias(libs.plugins.compose)
+    id("domain-plugin")
+}
+
+kotlin {
+    js(IR) {
+        browser()
+        binaries.executable()
+    }
+    sourceSets {
+        named("jsMain") {
+            dependencies {
+                implementation(compose.web.core)
+                implementation(compose.runtime)
+            }
+        }
+    }
+}
