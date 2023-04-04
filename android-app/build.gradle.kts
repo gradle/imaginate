ea87c81771ace24d424fcf54d3d30b07f5a9cc41
Paul Merlin <paul@gradle.com>
2023-04-04 11:39:52 +0200

    Fix generated android resource folder registration

--- android-app/build.gradle.kts
+++ android-app/build.gradle.kts
@@ -14,34 +14,17 @@ dependencies {
 
 val generatedResDir = layout.buildDirectory.dir("generated-res")
 
-val launcherIcon = tasks.register("launcherIcon", Copy::class) {
+val launcherIcon = tasks.register("launcherIcon", Sync::class) {
     from(sharedIcon)
     include("*.xml")
     into(generatedResDir.get().dir("drawable-anydpi-v26").asFile)
-    rename { "ic_launcher.xml" }
 }
 
 android {
     namespace = "conf.android"
-    sourceSets {
-        getByName("main") {
-            res {
-                srcDir(files(generatedResDir) {
-                    builtBy(launcherIcon)
-                })
-            }
-        }
-    }
-}
-
-// TODO this is a workaround, fixme!
-afterEvaluate {
-    listOf(
-        "processDebugResources", "mergeDebugResources", "mapDebugSourceSetPaths",
-        "processReleaseResources", "mergeReleaseResources", "mapReleaseSourceSetPaths",
-    ).forEach { taskName ->
-        tasks.named(taskName) {
-            dependsOn(launcherIcon)
-        }
+    applicationVariants.all {
+        registerGeneratedResFolders(files(generatedResDir) {
+            builtBy(launcherIcon)
+        })
     }
 }
