d13a7d31050de6e89af964f92e36ddef9a44aa18
Paul Merlin <paul@gradle.com>
2023-04-04 17:18:34 +0200

    Document kotlin extensions for easier plugin dependency declaration in buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -21,6 +21,8 @@ dependencies {
     implementation(libs.plugins.compose)
 }
 
+// This is a future Gradle feature to make declaring dependencies to plugins simpler
+// See an upvote https://docs.google.com/document/d/1P7aTeeVNhkhwxcS5sQNFrSsmqJOhDo3G8kUdhtp_vyM
 fun DependencyHandler.implementation(pluginDependency: Provider<PluginDependency>): Dependency? =
     add("implementation", pluginDependency.map {
         "${it.pluginId}:${it.pluginId}.gradle.plugin:${it.version.requiredVersion}"
