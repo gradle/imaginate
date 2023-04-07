222a83d1a122cfc7a6dd8025ac649d01f7d76665
Paul Merlin <paul@gradle.com>
2023-04-07 16:55:57 +0200

    Upgrade to compose 1.4.0-rc03
    compatible with Kotlin 1.8.20

--- buildSrc/src/main/kotlin/kotlin-compose-component.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-compose-component.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 }
 
 compose {
+    // Only needed for using a compose compiler plugin from an unreleased version
     // See https://androidx.dev/storage/compose-compiler/repository
-    kotlinCompilerPlugin = "androidx.compose.compiler:compiler:1.4.4-dev-k1.8.20-f6ae19e64ff"
+    // kotlinCompilerPlugin = "androidx.compose.compiler:compiler:1.4.4-dev-k1.8.20-f6ae19e64ff"
 }
