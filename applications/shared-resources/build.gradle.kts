37d8848e8ce6c4359683c1049aac2d66c889942a
Paul Merlin <paul@gradle.com>
2023-04-11 20:42:08 +0200

    Add generated logo

--- applications/shared-resources/build.gradle.kts
+++ applications/shared-resources/build.gradle.kts
@@ -4,7 +4,11 @@ plugins {
 
 generatedImages {
     register("icon") {
-        prompt = "round logo that mixes Gradle and Kotlin brands with transparent background and space around it"
+        prompt = "round icon that mixes Gradle and Kotlin brands with transparent background and space around it"
+        width = 512
+    }
+    register("logo") {
+        prompt = "hexagonal logo that mixes Gradle and Kotlin brands with transparent background and space around it"
         width = 512
     }
 }
