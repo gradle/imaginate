4ae2c66089b61932d41bac7f2459a92f6842a6d9
Paul Merlin <paul@gradle.com>
2023-06-27 14:21:38 +0200

    Upgrade Gradle, plugins and libraries

--- buildSrc/src/main/kotlin/imaginate/image_android_drawable.kt
+++ buildSrc/src/main/kotlin/imaginate/image_android_drawable.kt
@@ -92,7 +92,7 @@ abstract class DrawAndroidImageWork : WorkAction<DrawAndroidImageParameters> {
         xmlFile.parentFile.mkdirs()
         xmlFile.outputStream().use { output ->
             val errors: String = try {
-                Svg2Vector.parseSvgToXml(svgFile, output)
+                Svg2Vector.parseSvgToXml(svgFile.toPath(), output)
             } catch (e: Exception) {
                 throw Exception("Unable to parse svg file '$svgFile'", e)
             }
