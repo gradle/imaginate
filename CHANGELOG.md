ea9d007943883c173446c35d340af8843c17c26a
Paul Merlin <paul@gradle.com>
2023-06-29 17:27:14 +0200

    Revert "Upgrade Gradle, plugins and libraries"
    This reverts commit 4ae2c66089b61932d41bac7f2459a92f6842a6d9.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -22,8 +22,6 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Changed
 
 - The build now requires Java 17.
-- Upgraded Gradle from 8.1.1 to 8.2.
-- Upgraded plugins and libraries. Most significant are Kotlin from 1.8.20 to 1.9.0, Android from 7.4.2 to 8.1.0, Compose Multiplatform from 1.4.0 to 1.4.8.
 - On macos the build now requires cocoapods.
 
 // ### Removed
--- buildSrc/src/main/kotlin/kotlin-compose-component.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-compose-component.gradle.kts
@@ -1,11 +1,9 @@
-import imaginate.libs
-import imaginate.composeCompiler
-
 plugins {
     id("org.jetbrains.compose")
 }
 
 compose {
+    // Only needed for using a compose compiler plugin from an unreleased version
     // See https://androidx.dev/storage/compose-compiler/repository
-    kotlinCompilerPlugin = "androidx.compose.compiler:compiler:${libs.composeCompiler}"
+    // kotlinCompilerPlugin = "androidx.compose.compiler:compiler:1.4.4-dev-k1.8.20-f6ae19e64ff"
 }
